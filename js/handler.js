dca0f5ec8260849c2ad399796230ccef86d95cbe
KoshiNoLimit <zmsh04@gmail.com>
2018-09-11 22:20:43 +0300

    add

--- js/handler.js
+++ js/handler.js
@@ -4,20 +4,34 @@
 const instruments = [];
 
 /*ID'шники инструментов*/
-const ids = ['cursor', 'hand', 'plus', 'text',
-    'line', 'triangle', 'ellipse', 'rect',
-    'polygon', 'brush', 'eraser', 'pipette', 'zoom', 'color'];
+const ids = ['cursor', 'hand', 'pen', 'text',
+    'line', 'ellipse', 'rect', 'polygon',
+    'brush', 'eraser', 'pipette', 'zoom'];
 
 /*Текущий выбранный инструмент*/
-let currentInstrument;
+let currentInstrument = null;
+
+/*Текущая выделенная фигура*/
+let currentFigure = null;
+
+/*Захвачена ли опорная точка некоторой фигуры*/
+let someFigureTaken = false;
 
 const drawPanel = document.getElementById('draw-panel');
-const svgPanel = document.getElementById('svg-panel');
+const leftPanel = document.getElementById('left-panel');
+let svgPanel = document.getElementById('svg-panel');
 const svgNS = 'http://www.w3.org/2000/svg';
 
 /*Добавление всех инструментов в массив и присваивание обработчиков*/
 for (let i = 0; i < ids.length; i++) {
     instruments[i] = document.getElementById(ids[i]);
-    instruments[i].addEventListener("click", function() { currentInstrument = this; });
-    instruments[i].addEventListener("click", changeLabelSelected);
+    instruments[i].addEventListener('click', function() { currentInstrument = this; });
+    instruments[i].addEventListener('click', changeLabelSelected);
+    instruments[i].addEventListener('click', showOptions);
 }
+
+let buttonColor = document.getElementById('button-color');
+let colorPicker = document.getElementById('color-picker');
+buttonColor.addEventListener('click', function() { colorPicker.classList.toggle('show-option'); });
+
+let paletteColor = '#000000';
